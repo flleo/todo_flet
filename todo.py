4245cc3ef4bf08c4f0c704f2aca2befb9f6569af
flleo <admin@fedelleos.es>
2023-09-01 19:42:40 +0100

    nopersist

---
+++ todo.py
@@ -0,0 +1,177 @@
+import flet as ft
+
+
+class TodoApp(ft.UserControl):
+    def __init__(self):
+        super().__init__()
+        self.tasks = None
+        self.new_task = None
+        self.filter = None
+        self.items_left = ft.Text("0 tareas pendientes")
+
+    def build(self):
+        self.new_task = ft.TextField(hint_text="Escribe una nueva tarea", expand=True)
+        self.tasks = ft.Column()
+        self.filter = ft.Tabs(
+            selected_index=0,
+            on_change=self.tabs_changed,
+            tabs=[
+                ft.Tab(text="Todas"),
+                ft.Tab(text="Pendientes"),
+                ft.Tab(text="Completadas")
+            ],
+        )
+
+        # Application's root control (i.e. "view") containing all other controls
+        return ft.Column(
+            width=600,
+            controls=[
+                ft.Row(
+                    controls=[
+                        self.new_task,
+                        ft.FloatingActionButton(icon=ft.icons.ADD, on_click=self.add_clicked),
+                    ],
+                ),
+                ft.Row(
+                    controls=[
+                        self.items_left,
+                    ],
+                ),
+                ft.Column(
+                    controls=[
+                        self.filter,
+                        self.tasks
+                    ],
+                ),
+            ],
+        )
+
+    def clear_clicked(self, e):
+        for task in self.tasks.controls[:]:
+            if task.completed:
+                self.task_delete(task)
+
+    def update(self):
+        status = self.filter.tabs[self.filter.selected_index].text
+        count = 0
+        for task in self.tasks.controls:
+            task.visible = (
+                    status == "Todas"
+                    or (status == "Pendientes" and task.completed is False)
+                    or (status == "Completadas" and task.completed)
+            )
+            if not task.completed:
+                count += 1
+        self.items_left.value = f"{count} tarea(s) pendientes"
+        super().update()
+
+    def add_clicked(self, e):
+        task = Task(self.new_task.value, self.task_status_change, self.task_delete)
+        self.tasks.controls.append(task)
+        self.new_task.value = ""
+        self.update()
+
+    def task_delete(self, task):
+        self.tasks.controls.remove(task)
+        self.update()
+
+    def task_status_change(self, e):
+        self.update()
+
+    def tabs_changed(self, e):
+        self.update()
+
+
+class Task(ft.UserControl):
+    def __init__(self, task_name, task_status_change, task_delete):
+        super().__init__()
+        self.completed = False
+        self.edit_view = None
+        self.display_view = None
+        self.edit_name = None
+        self.display_task = None
+        self.task_name = task_name
+        self.task_status_change = task_status_change
+        self.task_delete = task_delete
+
+    def build(self):
+        self.display_task = ft.Checkbox(value=False, label=self.task_name, on_change=self.status_changed)
+        self.edit_name = ft.TextField(expand=1)
+
+        self.display_task = ft.Checkbox(
+            value=False, label=self.task_name, on_change=self.status_changed
+        )
+
+        self.display_view = ft.Row(
+            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+            vertical_alignment=ft.CrossAxisAlignment.CENTER,
+            controls=[
+                self.display_task,
+                ft.Row(
+                    spacing=0,
+                    controls=[
+                        ft.IconButton(
+                            icon=ft.icons.CREATE_OUTLINED,
+                            tooltip="Edita",
+                            on_click=self.edit_clicked,
+                        ),
+                        ft.IconButton(
+                            ft.icons.DELETE_OUTLINE,
+                            tooltip="Borra",
+                            on_click=self.delete_clicked,
+                        ),
+                    ],
+                ),
+            ],
+        )
+
+        self.edit_view = ft.Row(
+            visible=False,
+            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+            vertical_alignment=ft.CrossAxisAlignment.CENTER,
+            controls=[
+                self.edit_name,
+                ft.IconButton(
+                    icon=ft.icons.DONE_OUTLINE_OUTLINED,
+                    icon_color=ft.colors.GREEN,
+                    tooltip="Actualiza",
+                    on_click=self.save_clicked,
+                ),
+            ],
+        )
+        return ft.Column(controls=[
+            self.display_view, self.edit_view])
+
+    def edit_clicked(self, e):
+        self.edit_name.value = self.display_task.label
+        self.display_view.visible = False
+        self.edit_view.visible = True
+        self.update()
+
+    def save_clicked(self, e):
+        self.display_task.label = self.edit_name.value
+        self.display_view.visible = True
+        self.edit_view.visible = False
+        self.update()
+
+    def delete_clicked(self, e):
+        self.task_delete(self)
+
+    def status_changed(self, e):
+        self.completed = self.display_task.value
+        self.task_status_change(self)
+
+
+def main(page: ft.Page):
+    page.title = "Gestor de Tareas"
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.update()
+
+    # Create application instance
+    todo = TodoApp()
+
+    # Add application's root control to the page
+    page.add(todo)
+
+
+ft.app(target=main)
